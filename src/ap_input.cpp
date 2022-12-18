9c08a125ef10d63a556d4b28d4e893ff515be038
Ljn0099 <cientifico1818@gmail.com>
2022-12-18 22:44:20 +0100

    Added Fix undefined behaviour #25 pull request of the fork

--- src/ap_input.cpp
+++ src/ap_input.cpp
@@ -125,9 +125,9 @@ static struct
  {SDLK_KP7,	TRUE,	'7',      '7',	   CTL_PAD7,	ALT_PAD7},
  {SDLK_KP8,	TRUE,	'8',      '8',	   CTL_PAD8,	ALT_PAD8},
  {SDLK_KP9,	TRUE,	'9',      '9',	   CTL_PAD9,	ALT_PAD9},
- {SDLK_WORLD_0,0,0,0,0},
- {SDLK_SPACE,   FALSE,  ' ', ' ', ' ', ' '}
-};
+ {SDLK_WORLD_0,FALSE,0,0,0,0},
+ {SDLK_SPACE,   FALSE,  ' ', ' ', ' ', ' '},
+ {SDLK_UNKNOWN,FALSE,0,0,0,0};
 #endif
 
 int processevent () { // Convert keys down to PDC key commands. Introduces priority if multiple keys are pressed.
